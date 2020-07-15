50ee2e92c2e60161aa77019da35c194b457af49e
pushpitbhardwaj <pushpit07@gmail.com>
2020-07-15 15:54:50 +0530

    Final Proj

--- telusko/settings.py
+++ telusko/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,6 +78,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'trial-db',
+        'USER': 'pushpitbhardwaj',
+        'PASSWORD': '12345',
+        'HOST': 'localhost'
     }
 }
 
@@ -122,3 +126,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
